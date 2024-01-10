b56eec645b372082b4d9c6d595584edf603b2d2c
Ath1thyan <athitasks@gmail.com>
2024-01-11 02:26:01 +0530

    JS Practice

--- JavaScript/Basics/Practice2/script.js
+++ JavaScript/Basics/Practice2/script.js
@@ -71,3 +71,49 @@ for (let i=0; i<100; i++){
     arr.push(i)
 }
 console.log(arr)
+
+let arr1 = []
+for (let i=0; i<100; i+=2){
+    arr1.push(i)
+}
+console.log(arr1)
+console.log(arr1[5])
+
+
+let arr2 = []
+for(let i=0; i<3; i++){
+    arr2.push([])
+    for(let j=0; j<3; j++){
+        arr2[i].push(j)
+    }
+}
+console.log(arr2)
+console.table(arr2)
+
+
+let arr3 = ['Athi', 'raj', 'sam', 'BMW']
+console.log("For loop : ")
+for (let i=0; i<arr3.length; i++){
+    console.log(arr3[i])
+}
+console.log("For of loop : ")
+for (let ar of arr3){
+    console.log(ar)
+}
+
+
+console.log("For in loop : ")
+let cars = {
+    Brand : "Volvo",
+    Model : "XC90",
+    Color : "Black"
+}
+for (let c in cars){
+    console.log(c + " : " + cars[c])
+}
+
+let arr_keys = Object.keys(cars)
+console.log(arr_keys)
+let arr_val = Object.values(cars)
+console.log(arr_val)
+
