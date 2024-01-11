275bb75648b1447369baee0399a248d506f99463
Ath1thyan <athitasks@gmail.com>
2024-01-12 01:34:35 +0530

    JS Practice

--- JavaScript/Basics/Practice3/script.js
+++ JavaScript/Basics/Practice3/script.js
@@ -24,3 +24,58 @@ for (const i of set2.values()){
 console.log(txt2)
 
 
+function add(a,b){
+    return a+b;
+}
+console.log(add(12,12))
+
+
+function sum(){
+    let total = 0;
+    for (let i=0; i<arguments.length; i++){
+        total += arguments[i];
+    }
+    return total;
+}
+console.log(sum)
+console.log(sum(12,12,12,12))
+
+function total(...args){
+    let total = 0;
+    for (let i=0; i<args.length; i++){
+        total += args[i];
+    }
+    return total;
+}
+console.log(total)
+console.log(total(12,12,12,12))
+
+const ad = function(a,b){
+    return a+b;
+}
+console.log(ad)
+console.log(ad(5,10))
+
+const addd = (a,b) => { return a+b; }
+console.log(addd)
+console.log(addd(10,2))
+
+let tot=0
+const addd1 = (...args) => { for(let i=0; i<args.length; i++){ tot+=args[i]} return tot; }
+console.log(addd)
+console.log(addd(1,2,3))
+
+let num = [1,2,3,4,5,6]
+console.log(num)
+let num1 = num.map(num => num*2)
+console.log(num1)
+
+let num2 = [1,2,3,4,5,6]
+console.log(num)
+let num3 = num.map(numb => numb*2)
+console.log(num3)
+
+let frt = ['apple','banana','cherry','kiwi','grape','orange']
+console.log(frt)
+const frt1 = frt.filter(fruit => fruit.length > 5)
+console.log(frt1)
