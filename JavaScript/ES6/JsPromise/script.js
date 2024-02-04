ec4ecbdc86e30c8cbad9b292baa726504d8ea157
Ath1thyan <athitasks@gmail.com>
2024-02-04 15:38:17 +0530

    ...

--- JavaScript/ES6/JsPromise/script.js
+++ JavaScript/ES6/JsPromise/script.js
@@ -32,3 +32,65 @@ function bookTickets(){
 // bookTicket.then((amt)=> console.log(`Booked successfully ${amt}`))
 bookTickets().then((amt)=> console.log(`Booked successfully ${amt}`))
 .catch(()=> console.log("Booking failed"))
+
+
+
+function tossCoin(){
+    return new Promise((resolve, reject) => {
+        const rand = Math.floor(Math.random()*2)
+
+        if(rand == 0){
+            resolve()
+        }
+        else{
+            reject()
+        }
+    })
+}
+
+tossCoin()
+.then(() =>  console.log("Heads"))
+.then(() =>  console.log("Heads"))
+.then(() =>  console.log("Heads"))
+.then(() =>  console.log("Heads"))
+.catch(() => console.log("Tails"));
+
+
+
+
+
+let reachA = new Promise((resolve, reject) => {
+    const reached = true
+    if(reached){
+        setTimeout(resolve, 3000, "A Reached Home")
+    }
+    else{
+        reject("A not reached")
+    }
+})
+let reachB = new Promise((resolve, reject) => {
+    const reached = true
+    if(reached){
+        setTimeout(resolve, 3000, "B Reached Home")
+    }
+    else{
+        reject("B not reached")
+    }
+})
+let reachC = new Promise((resolve, reject) => {
+    const reached = false
+    if(reached){
+        setTimeout(resolve, 3000, "C Reached Home")
+    }
+    else{
+        reject("C not reached")
+    }
+})
+
+Promise.all([reachA, reachB, reachC])
+.then((message) => console.log(message))
+.catch((error) => console.log(error))
+
+Promise.allSettled([reachA, reachB, reachC])
+.then((message) => console.log(message))
+.catch((error) => console.log(error))
